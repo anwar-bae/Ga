e9b1cb4ea716743351bd230ea1f059df761906d7
anwar-bae <54600783+anwar-bae@users.noreply.github.com>
2020-11-04 20:03:22 +0700

    Update button.js

--- button.js
+++ button.js
@@ -1,3 +1,3 @@
-document.write('<div class="page-header text-center">'+'<center>'+'<a href="http://fizmanrzn.com/register.php?sub_id=sapu_jagad">'+
+document.write('<div class="page-header text-center">'+'<center>'+'<a href="http://warfiz.com/rc?sub_id=sapu_jagad">'+
 '<img src="https://bit.ly/2xKJn6a" class="img-responsive">'+'<img src="https://bit.ly/39mx4dt" class="img-responsive">'+
 '</a>'+'</center>'+'</div>');
