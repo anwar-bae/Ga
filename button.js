88ac163b202889531d6e83eff63bcee5892b09ea
anwar-bae <54600783+anwar-bae@users.noreply.github.com>
2020-11-10 19:39:59 +0700

    Update button.js

--- button.js
+++ button.js
@@ -1,3 +1,3 @@
 document.write('<div class="page-header text-center">'+'<center>'+'<a href="http://warfiz.com/rc?sub_id=serngenge">'+
-'<img src="https://bit.ly/2xKJn6a" class="img-responsive">'+'<img src="https://bit.ly/39mx4dt" class="img-responsive">'+
+'<img src="https://bit.ly/2xKJn6a" class="img-responsive">'+'<img src="https://www.pasaz24cdn.pl/web3469/productimages%5C1d718540-3c2d-46d9-98c5-4d84bc5e98ed-rejestracja.png" class="img-responsive">'+
 '</a>'+'</center>'+'</div>');
